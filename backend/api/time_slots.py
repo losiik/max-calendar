2ffc0e9ac88cb57a1a73373f0274baab3f306c3f
losiik <49565548+losiik@users.noreply.github.com>
2025-11-09 00:25:41 +0300

    start 2

--- backend/api/time_slots.py
+++ backend/api/time_slots.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter, Depends
 from fastapi import HTTPException
 
 from backend.dependes import get_user_service
-from backend.schemas.user_schema import UserCreateRequest, UserCreateResponse
-from backend.services.user_service import UserService
+from backend.schemas.time_slots_schema import TimeSlotsCreateRequest
+from backend.facade.time_slots_facade import TimeSlotsFacade
 from backend.exceptions import UserAlreadyExistsError
 
 
@@ -13,8 +13,8 @@ time_slots_router.tags = ["Time Slots"]
 
 @time_slots_router.put('/', response_model=UserCreateResponse)
 async def book_time_slot(
-        user_data: UserCreateRequest,
-        user_service: UserService = Depends(get_user_service)
+        booking_data: TimeSlotsCreateRequest,
+        time_slots_facade: TimeSlotsFacade = Depends(get_user_service)
 ):
     pass
 
