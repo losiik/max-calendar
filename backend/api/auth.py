9c41781605910304f14881f8cac522effb645b57
losiik <49565548+losiik@users.noreply.github.com>
2025-11-14 12:09:05 +0300

    fix

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -105,18 +105,3 @@ def validate_max_webapp_data(init_data: str, bot_token: str) -> dict[str, Any]:
         "params": params,
         "correct": calculated_hash == received_hash
     }
-
-
-if __name__ == "__main__":
-    # Пример данных из документации
-    test_init_data = "ip=79.104.38.102&user=%7B%22id%22%3A5933603%2C%22first_name%22%3A%22%D0%A1%D0%B5%D1%80%D0%B3%D0%B5%D0%B9%22%2C%22last_name%22%3A%22%22%2C%22username%22%3Anull%2C%22language_code%22%3A%22ru%22%2C%22photo_url%22%3A%22https%3A%2F%2Fi.oneme.ru%2Fi%3Fr%3DBTGBPUwtwgYUeoFhO7rESmr81M_4nx97oU2nFSrj5R6ZuVxrYpcuZYQ8DeRx33ZwTvM%22%7D&query_id=9ba0888b-b504-4251-a6ef-8a4d0d1d44fd&auth_date=1763078704&hash=1a48a6ec6af4c9433335bc0349958e1b9d45d9cdf655a35e84d7c326ec9b38e1&chat=%7B%22id%22%3A74771308%2C%22type%22%3A%22DIALOG%22%7D"
-    test_bot_token = "f9LHodD0cOK71zkbeDvAE2bYxTfQpnLiG3QbVAmq0acBjJxI7Bc1VnBOjJzYLwtLqYPL4wtxA2NfBXuftlRv"
-
-    result = validate_max_webapp_data(test_init_data, test_bot_token)
-    print(result)
-    print(f"Валидация: {'Успешна' if result else 'Неуспешна'}")
-
-    # Пример с подделанными данными
-    fake_init_data = test_init_data.replace("400", "999")
-    result_fake = validate_max_webapp_data(fake_init_data, test_bot_token)
-    print(f"Валидация подделанных данных: {'Успешна' if result_fake else 'Неуспешна'}")
